3f771efb343d9db34dcacfa7d1de256e32da8c16
Nguyễn Khắc Trường <johnkt12qz@gmail.com>
2022-12-19 15:31:51 +0700

    Update config for package distribution

--- setup.py
+++ setup.py
@@ -1,19 +1,4 @@
-from setuptools import (
-    setup,
-    find_packages,
-)
+from setuptools import setup, find_packages
 
-
-with open("README.md") as f:
-    readme = f.read()
-
-setup(
-    name="Music Classification",
-    version="0.1.0",
-    description="Make use of ID3 algorithm to build a decision tree for categorising musics",
-    long_description=readme,
-    author="Nguyễn Khắc Trường",
-    author_email="johnkt12qz@gmail.com",
-    url="https://github.com/pseudoCode34/MusicClassification",
-    packages=find_packages(exclude=("tests", "docs")),
-)
+if __name__ == "__main__":
+    setup()
